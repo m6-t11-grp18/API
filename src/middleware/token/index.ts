d8b290cb11e6305b7f44288a3eef92e3a8b663a5
Fabiojcp <fabio50019@gmail.com>
2022-12-15 01:28:10 -0300

    feat: prettier config in index´s

--- src/middleware/token/index.ts
+++ src/middleware/token/index.ts
@@ -1,25 +1,32 @@
-import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
-import { BadRequestError, UnauthorizedError } from "../../utils/error";
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import {
+  BadRequestError,
+  UnauthorizedError,
+} from '../../utils/error';
 
 class TokenMiddleware {
-  async customer (req: Request, res: Response, next: NextFunction) {
+  async customer(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     let token = req.headers.authorization;
 
     if (!token) {
       throw new BadRequestError(
-        "to access this route you need to send a token"
+        'to access this route you need to send a token'
       );
     }
 
-    token = token.split(" ")[1];
+    token = token.split(' ')[1];
 
     jwt.verify(
       token as string,
       process.env.SECRET_KEY as string,
       (error: any, decoded: any) => {
         if (error) {
-          throw new UnauthorizedError("Invalid Token");
+          throw new UnauthorizedError('Invalid Token');
         }
         req.user = {
           id: decoded.sub,
@@ -31,27 +38,33 @@ class TokenMiddleware {
     );
   }
 
-  async manager(req: Request, res: Response, next: NextFunction) {
+  async manager(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     let token = req.headers.authorization;
 
     if (!token) {
       throw new BadRequestError(
-        "to access this route you need to send a token"
+        'to access this route you need to send a token'
       );
     }
 
-    token = token.split(" ")[1];
+    token = token.split(' ')[1];
 
     jwt.verify(
       token as string,
       process.env.SECRET_KEY as string,
       (error: any, decoded: any) => {
         if (error) {
-          throw new UnauthorizedError("Invalid Token");
+          throw new UnauthorizedError('Invalid Token');
         }
 
         if (!decoded.isAdm) {
-          throw new UnauthorizedError("Invalid credentials");
+          throw new UnauthorizedError(
+            'Invalid credentials'
+          );
         }
 
         req.user = {
